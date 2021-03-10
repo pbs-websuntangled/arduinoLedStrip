78acaf470937ad10551f3e344e1c038ece68557b
PBS <pbs.websuntangled@gmail.com>
2021-03-10 16:53:40 +0000

    wip

--- project_x/ultrasonic_01.ino
+++ project_x/ultrasonic_01.ino
@@ -6,18 +6,36 @@
 // Tested on 17 September 2019
 // ---------------------------------------------------------------- //
 
-#define echoPin_1 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin_1 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+// attach pin D2 Arduino to pin Echo of HC-SR04
+//attach pin D3 Arduino to pin Trig of HC-SR04
+#define echoPin_1 2 
+#define trigPin_1 3 
 
-#define echoPin_2 4 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin_2 5 //attach pin D3 Arduino to pin Trig of HC-SR04
+// attach pin D2 Arduino to pin Echo of HC-SR04
+//attach pin D3 Arduino to pin Trig of HC-SR04
+#define echoPin_2 4 
+#define trigPin_2 5 
+
+// attach pin D2 Arduino to pin Echo of HC-SR04
+//attach pin D3 Arduino to pin Trig of HC-SR04
+#define echoPin_3 6 
+#define trigPin_3 7 
 
 // defines variables
-long duration; // variable for the duration of sound wave travel
-int distance;  // variable for the distance measurement
+// variable for the duration of sound wave travel
+long duration; 
+// variable for the distance_sensor_1 measurement
+int distance_sensor_1;  
+int distance_sensor_2;  
+int distance_sensor_3;  
+int counter;
 
 void setup()
 {
+
+    // set the counter to zero 
+    counter = 0;
+
     // Sets the trigPin_1 as an OUTPUT
     // Sets the echoPin_1 as an INPUT
     pinMode(trigPin_1, OUTPUT);                       
@@ -27,30 +45,37 @@ void setup()
     pinMode(trigPin_2, OUTPUT);                       
     pinMode(echoPin_2, INPUT);   
 
-    Serial.begin(9600);                               // // Serial Communication is starting with 9600 of baudrate speed
+    // Sets the trigPin_1 as an OUTPUT
+    pinMode(trigPin_3, OUTPUT);                       
+    pinMode(echoPin_3, INPUT);   
+
+    Serial.begin(115200);                               // // Serial Communication is starting with 9600 of baudrate speed
     Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
     Serial.println("with Arduino Mega 2560");
 }
 void loop()
 {
-    // Clears the trigPin_1 condition
-    digitalWrite(trigPin_1, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin_1 HIGH (ACTIVE) for 10 microseconds
-    digitalWrite(trigPin_1, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin_1, LOW);
-    // Reads the echoPin_1, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin_1, HIGH);
-    // Calculating the distance
-    // Speed of sound wave divided by 2 (go and back)
-    distance = duration * 0.34 / 2;
-    // Displays the distance on the Serial Monitor
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.println(" mm");
+    // keep a counter so I can see how fast the data is transferr
+    Serial.print(" ===============  ");
+    Serial.print(counter);
+    Serial.println(" ===============  ");
 
-    int distance = get_distance(echoPin_2, trigPin_2);
+    distance_sensor_1 = get_distance(echoPin_1, trigPin_1);
+    distance_sensor_2 = get_distance(echoPin_2, trigPin_2);
+    distance_sensor_3 = get_distance(echoPin_3, trigPin_3);
+
+    Serial.print("distances in mm: ");
+    Serial.print(distance_sensor_1);
+    Serial.print(", ");
+    Serial.print(distance_sensor_2);
+    Serial.print(", ");
+    Serial.print(distance_sensor_3);
+    Serial.println("");
+    Serial.println("");
+
+    // increment the counter
+    counter = counter + 1;
+
 }
 
 int get_distance(int echoPin, int trigPin)
@@ -64,13 +89,10 @@ int get_distance(int echoPin, int trigPin)
     digitalWrite(trigPin, LOW);
     // Reads the echoPin_1, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
-    // Calculating the distance
+    // Calculating the distance_sensor_1
     // Speed of sound wave divided by 2 (go and back)
-    distance = duration * 0.34 / 2;
-    Serial.print("Trigger pin: ");
-    Serial.print(trigPin_1);
-    Serial.print("Distance from function: ");
-    Serial.print(distance);
-    Serial.println(" mm");
-    return distance;
+    int distance_sensor = duration * 0.34 / 2;
+
+    delayMicroseconds(200);
+    return distance_sensor;
 }
